8f2ea009b74552949939e57e672981b787418e09
kktaia <65294191+kktaia@users.noreply.github.com>
2021-09-15 13:29:35 +0900

    変えたよ～

--- main.rb
+++ main.rb
@@ -3,9 +3,8 @@ require 'dxruby'
 player_img=Image.load("image/player.png")
 x=100
 y=100
+player=Sprite.new(x,y,player_img)
 
 Window.loop do
-    Window.draw(x, y, player_img)
-    x+=Input.x
-    y+=Input.y
+  player.x+=Input.x
 end
