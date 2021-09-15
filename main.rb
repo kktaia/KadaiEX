52c9f38b1713eec3dcdf29c60e4624f07e30c397
kktaia <65294191+kktaia@users.noreply.github.com>
2021-09-15 14:33:15 +0900

    変えたよ～

--- main.rb
+++ main.rb
@@ -1,10 +1,11 @@
-require 'dxruby'
+require "dxruby"
 
-player_img=Image.load("image/player.png")
-x=100
-y=100
-player=Sprite.new(x,y,player_img)
+player_img = Image.load('image/player.png')
+x = 100
+y = 100
+player = Sprite.new(x, y, player_img)
 
 Window.loop do
-  player.x+=Input.x
+  player.x += Input.x
+  player.y+=Input.y
 end
