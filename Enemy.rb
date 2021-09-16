af3821679321f1a4fb0fc0720b2e836559fd5d9a
kktaia <65294191+kktaia@users.noreply.github.com>
2021-09-17 01:52:31 +0900

    変えたよ～

--- Enemy.rb
+++ Enemy.rb
@@ -3,8 +3,6 @@ require_relative "def"
 class Enemy < Sprite
   def initialize(x,y,image)
     super
-    self.scale_x *= (CELL_SIZE/width.to_f)
-    self.scale_y *= (CELL_SIZE/height.to_f)
   end
   def hit
     self.vanish
--- Player.rb
+++ Player.rb
@@ -6,32 +6,30 @@ class Player < Sprite
   def initialize(x, y, image)
     @score = 0
     super
-    self.scale_x *= (CELL_SIZE/width.to_f)
-    self.scale_y *= (CELL_SIZE/height.to_f)
   end
 
   def update
-    limit = 12*80
+    limit = 12 * 80
     self.x += Input.x * 5
     self.y += Input.y * 5
-    self.x = (x < 0 ? 0 : limit - width) if x < 0 || x > limit - width
-    self.y = (y < 0 ? 0 : limit - height) if y < 0 || y > limit - height
-    
-    if x <= Window.width / 2
-      Window.ox = 0
+    self.x = (x.negative? ? 0 : limit - width) if x.negative? || x > limit - width
+    self.y = (y.negative? ? 0 : limit - height) if y.negative? || y > limit - height
+
+    Window.ox = if x <= Window.width / 2
+      0
     elsif x >= limit - Window.width / 2
-      Window.ox = limit - Window.width
+      limit - Window.width
     else
-      Window.ox = x - Window.width / 2
-    end
-      
-    if y <= Window.height / 2
-      Window.oy = 0
+      x - Window.width / 2
+                end
+
+    Window.oy = if y <= Window.height / 2
+      0
     elsif y >= limit - Window.height / 2
-      Window.oy = limit - Window.height
+      limit - Window.height
     else
-      Window.oy = y - Window.height / 2
-    end
+      y - Window.height / 2
+                end
   end
 
   def shot
